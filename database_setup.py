9f0b8c0895e9a33f4cac807843f43caef6ae99f1
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-05-04 18:29:28 -0700

    Complete Database_setup file

---
+++ database_setup.py
@@ -0,0 +1,26 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+    _tablename_ = 'categories'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+class Item(Base):
+    _tablename_ = 'items'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+engine = create_engine(
+    'sqlite://itemcatalog.db')
+
+Base.metadata.create_all(engine)
